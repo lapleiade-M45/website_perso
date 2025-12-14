127bac90e044f75dfba975f6234bf8b9d95d0e0a
cb <cdricw3l@gmail.com>
2025-12-14 01:24:44 +0100

    css

--- src/logic/tree.ts
+++ src/logic/tree.ts
@@ -17,26 +17,24 @@ function get_node(obj: TreeData): Treenode_t
     return(d)
 }
 
-function count_children(tree: Tree_t, node_level: number): number
-{
-    let children: number = 0;
-
-    for(const node of tree.tree)
-    {
-        if(node.level == node_level + 1)
-            children++;
-    }
-    return(children);
-}
+/* 
+    Pour determiner le nombre de children , je compare le path de mon noeud courant avec le path du noeud_source sur sizeof(char * noeud_source_len) bytes.
+    Si les path son egaux, j'incremente le compteur.
+    En fin de processus, je retire 1 au compte correspondant au path du current_noeud dans l'arbre
+*/
 
 function set_children(tree: Tree_t)
 {
-    for (const node of tree.tree)
+    for (const current_node of tree.tree)
     {
+        let children: number = 0;
 
-        
-
-        node.childen_count = count_children(tree, node.level);
+        for(const node of tree.tree)
+        {
+            if(node.obj.path.slice(0, current_node.obj.path.length) == current_node.obj.path)
+                children++;
+        }
+        current_node.childen_count = children - 1;
     }
 }
 
